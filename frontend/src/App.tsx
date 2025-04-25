447e57c23693bf6223dd50d212e9f589faa918b8
unknown <syedghazali>
2025-04-25 08:46:02 +0500

    feat: signup/login form added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,15 @@ import './App.css'
 import { Route, Routes } from 'react-router'
 import Home from './Home/Home'
 import HeroSection from './Hero-Section/HeroSection'
+import { LoginForm } from './components/login-form'
+import { SignupForm } from './components/sign-up'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HeroSection />}></Route>
+      <Route path='/login' element={<LoginForm />}></Route>
+      <Route path='/sign-up' element={<SignupForm />}></Route>
       {/* <Route path='/' element={<Home />}></Route> */}
     </Routes>
   )
